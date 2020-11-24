50ac211638937592fc2d855b664f77bb1dc56bed
soeren <soeren@DESKTOP-NCU3OCN>
2020-11-24 23:38:34 +0100

    AgarIO Test Project

---
+++ AgarIO with Engine/src/game/PlayerObject.java
@@ -0,0 +1,80 @@
+package game;
+
+import java.awt.Color;
+import java.util.ArrayList;
+
+import engine.game.GameContainer;
+import engine.gameobjects.GameObject;
+import engine.gameobjects.gamebehaviour.GameBehaviour;
+import engine.math.Vector2;
+import engine.scenes.SceneManager;
+
+public class PlayerObject extends GameBehaviour {
+
+	private int size;
+	private PlayerObject leader;
+	private ArrayList<PlayerObject> childs;
+	
+	private Vector2 velocity;
+	
+	public PlayerObject(int size, PlayerObject leader) {
+		this.velocity = new Vector2(0);
+		this.setSize(size);
+		this.leader = leader;
+		if (leader == null) { //No leader => Iam the leader 
+			childs = new ArrayList<PlayerObject>();
+			this.leader = this;
+		}
+	}
+	
+	public void setSize(int size) {
+		this.size = size;
+	}
+	
+	public void addSize(int size) {
+		this.size += size;
+	}
+	
+	public int getSize() {
+		return this.size;
+	}
+	
+	public void addForce(Vector2 force) {
+		this.velocity.add(force);
+	}
+	
+	public void update() {
+		if (GameContainer.input.isButton(1)) {
+			split();
+		}
+		
+		Vector2 movingVec = new Vector2(GameContainer.input.getMousePosToWorld(), this.gameObject.getTransformWithCaution().position);
+		velocity.add(Vector2.multiply(movingVec, (float)GameContainer.dt / 10));
+		
+		for (PlayerObject po : this.leader.childs) {
+			Vector2 distanceVec = new Vector2(this.gameObject.getTransformWithCaution().position, po.gameObject.getTransformWithCaution().position);
+			float minDistance = (this.size + po.size) / 2;
+			float distance = distanceVec.length();
+			
+			if (distance <= minDistance) {
+				this.gameObject.addPosition(movingVec);
+			}
+		}
+		
+		velocity.normalize();
+		this.gameObject.addPosition(velocity);
+	}
+	
+	private void split() {
+		GameObject newSplit = new GameObject(Vector2.add(this.gameObject.getTransformWithCaution().position, new Vector2(200)));
+		newSplit.addComponent(new PlayerObject((int)(this.size), this.leader));
+		this.setSize(this.size);
+		SceneManager.activeScene.addGameObject(newSplit);
+		this.leader.childs.add(newSplit.getComponent(PlayerObject.class));
+	}
+	
+	public void render() {
+		this.d.setColor(this.leader == this ? Color.PINK : Color.WHITE);
+		this.d.fillCircle(new Vector2(size));
+	}
+}
---
+++ AgarIO with Engine/src/main/Main.java
@@ -0,0 +1,78 @@
+package main;
+
+import java.awt.event.KeyEvent;
+
+import engine.game.AbstractGame;
+import engine.game.GameContainer;
+import engine.gameobjects.GameObject;
+import engine.gameobjects.gamebehaviour.Camera;
+import engine.gameobjects.gamebehaviour.GameBehaviour;
+import engine.math.Vector2;
+import engine.scenes.Scene;
+import engine.scenes.SceneManager;
+import game.PlayerObject;
+
+public class Main extends AbstractGame {
+	
+	Scene inGame;
+	
+	public static void main(String[] args) {
+		GameContainer gc = new GameContainer(new Main());	
+		gc.setSize(new Vector2(1000, 500));
+		gc.start();
+	}
+	
+	public Main() {
+		inGame = new Scene("InGame") {
+			
+			Camera cam;
+
+			@Override
+			public void instanceGameObjects() {
+				GameObject player = new GameObject(new Vector2(0));
+				player.addComponent(new PlayerObject(100, null));
+				inGame.addGameObject(player);
+				
+				GameObject camera = new GameObject(new Vector2(0));
+				cam = new Camera();
+				camera.addComponent(cam);	
+				camera.addComponent(new GameBehaviour() {
+					public void update() {
+						if (GameContainer.input.isKey(KeyEvent.VK_LEFT)) {
+							this.gameObject.addPosition(new Vector2(-1,0));
+						} else if (GameContainer.input.isKey(KeyEvent.VK_RIGHT)) {
+							this.gameObject.addPosition(new Vector2(1,0));
+						} else if (GameContainer.input.isKey(KeyEvent.VK_UP)) {
+							this.gameObject.addPosition(new Vector2(0,-1));
+						} else if (GameContainer.input.isKey(KeyEvent.VK_DOWN)) {
+							this.gameObject.addPosition(new Vector2(0,1));
+						}
+					}
+				});
+				inGame.addGameObject(camera);
+			}
+			
+			public void sceneLoaded() {
+				cam.view();
+			}
+		};
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void start() {
+		SceneManager.loadScene(inGame);
+		
+	}
+
+	@Override
+	public void update() {
+		Camera.activeCam.zoom += GameContainer.input.getScroll() / 50f;
+		
+	}
+}
